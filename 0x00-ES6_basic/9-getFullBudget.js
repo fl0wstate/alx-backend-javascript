be742c1c1ae360a31d0ad86ae215e6022ec461c7
gh057 <victormurithi2020@gmail.com>
2024-05-28 19:00:29 +0300

    having issues with my test cases

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,13 +1,13 @@
-import getBudgetObject from './7-getBudgetObject';
+import getBudgetObject from "./7-getBudgetObject";
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: ()=> {
+    getIncomeInDollars: function () {
       return `$${this.income}`;
     },
-    getIncomeInEuros: ()=> {
+    getIncomeInEuros: function () {
       return `${this.income} euros`;
     },
   };
