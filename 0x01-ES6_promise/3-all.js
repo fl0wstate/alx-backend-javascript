f8644ff478f9d7a30f3b63fa59fd41d67fd9df26
gh057 <victormurithi2020@gmail.com>
2024-05-30 05:28:01 +0300

    removing the export infront of the function name

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   const profileObject = uploadPhoto();
   const userInfo = createUser();
 
