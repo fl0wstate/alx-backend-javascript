e28b6322fafdf170d416890e832416550ef0ccea
gh057 <victormurithi2020@gmail.com>
2024-05-31 18:18:12 +0300

    treat the function as async function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export default async function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((data) => console.log(data[0].body, data[1].firstName, data[1].lastName))
     .catch(() => console.log('Signup system offline'));
