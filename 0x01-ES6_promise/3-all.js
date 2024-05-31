f3e763f9a9cfa23b4b86d5f89f38fa6758b6aab6
gh057 <victormurithi2020@gmail.com>
2024-05-31 18:02:11 +0300

    adding a promise object at the end

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,8 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((data) => {
-      console.log(data[0].body, data[1].firstName, data[1].lastName);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+  const promiseObject = Promise.all([uploadPhoto(), createUser()])
+    .then((data) => console.log(data[0].body, data[1].firstName, data[1].lastName))
+    .catch(() => console.log('Signup system offline'));
+  return promiseObject;
 }
