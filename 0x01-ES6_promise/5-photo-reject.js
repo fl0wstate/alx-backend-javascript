b06848a914cf1f2257268d295ace6336d5a69cb0
gh057 <victormurithi2020@gmail.com>
2024-05-31 17:47:29 +0300

    rejects are also turned into return values

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 export default function uploadPhoto(filename) {
   if (filename && typeof filename === 'string') {
-    return new Promise((_, reject) => {
+    Promise((_, reject) => {
       reject(new Error(`${filename} cannot be processed`));
     });
   }
