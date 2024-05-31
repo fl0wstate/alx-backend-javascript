5650da96814e4eef204efe26be83b37de674091c
gh057 <victormurithi2020@gmail.com>
2024-05-31 17:42:27 +0300

    returning a reject promise

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,7 @@
+export default function uploadPhoto(filename) {
+  if (filename && typeof filename === "string") {
+    return new Promise((_, reject) => {
+      reject(new Error(`${filename} cannot be processed`));
+    });
+  }
+}
