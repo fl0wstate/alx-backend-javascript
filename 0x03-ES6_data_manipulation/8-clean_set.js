455074a863633a539a9fd764df4d3607191da3ab
gh057 <victormurithi2020@gmail.com>
2024-06-05 10:32:06 +0300

    catching any error  and throwning off an empty string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,14 +2,18 @@ export default function cleanSet(set, startString) {
   if (!set || !startString || typeof startString !== 'string') {
     return '';
   }
-  let res = '';
-  for (const elem of set) {
-    if (elem.startsWith(startString)) {
-      if (res.length > 0) {
-        res += '-';
+  try {
+    let res = '';
+    for (const elem of set) {
+      if (elem.startsWith(startString)) {
+        if (res.length > 0) {
+          res += '-';
+        }
+        res += elem.slice(startString.length);
       }
-      res += elem.slice(startString.length);
     }
+    return res;
+  } catch (_) {
+    return '';
   }
-  return res;
 }
