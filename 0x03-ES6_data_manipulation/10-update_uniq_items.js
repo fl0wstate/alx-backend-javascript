153aeacf8eadbd3bc34acae2dd18087e324eb1c9
gh057 <victormurithi2020@gmail.com>
2024-06-05 10:28:34 +0300

    catch the error when something goes wrong

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,13 +2,13 @@ export default function updateUniqueItems(maps) {
   if (!maps) {
     throw new Error('Cannot process');
   }
-  maps.forEach((val, key) => {
-    if (val === 1) {
-      maps.set(key, 100);
-    } else if (!maps.has(key)) {
-      throw new Error('Cannot process');
-    }
-  });
-
-  return maps;
+  try {
+    maps.forEach((val, key) => {
+      if (val === 1) {
+        maps.set(key, 100);
+      }
+    });
+  } catch (_) {
+    throw new Error('Cannot process');
+  }
 }
