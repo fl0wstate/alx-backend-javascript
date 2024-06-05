b03444b44a0495e8835edd1d319343273b40fac6
gh057 <victormurithi2020@gmail.com>
2024-06-05 07:15:19 +0300

    slicing in javascript

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  if (!set || !startString || typeof startString !== 'string') {
+    return '\n';
+  }
+  let res = '';
+  for (const elem of set) {
+    if (elem.startsWith(startString)) {
+      res = res.concat('-', elem.slice(startString.length));
+    }
+  }
+  return res.slice(1);
+}
