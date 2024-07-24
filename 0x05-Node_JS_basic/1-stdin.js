0d1b7c92f899e2bc1a5f8a8d7eda9d17d48542af
gh057 <victormurithi2020@gmail.com>
2024-07-24 12:19:06 +0300

    correct indentation

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit();
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.exit();
 });
 
 if (process.stdin.isTTY) {
   process.on('exit', () => {
-      process.stdout.write('This important software is now closing\n');
+    process.stdout.write('This important software is now closing\n');
   });
 }
