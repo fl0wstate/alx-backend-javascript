d5c1899a0967cc61af34efcde3fd8f1a5a774de4
gh057 <victormurithi2020@gmail.com>
2024-07-24 03:40:53 +0300

    cover everything under the try catch method

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,35 +2,38 @@ const fs = require('fs');
 const readline = require('readline');
 
 module.exports = function countStudents(filepath) {
-  // Make a stream
-  const stream = fs.createReadStream(filepath);
-  
-  stream.on('error', (_) => {
-    throw new Error("Cannot load the database");
-  });
+  try {
+    const stream = fs.createReadStream(filepath);
+    
+    stream.on('error', (_) => {
+      throw new Error("Cannot load the database");
+    });
 
-  const rl = readline.createInterface({input: stream});
-  const data_holder = [];
+    const rl = readline.createInterface({input: stream});
+    const data_holder = [];
 
-  rl.on('line', (row) => {
-    if (row.trim()) {
-      data_holder.push(row.split(","));
-    }
-  });
-
-  rl.on('close', () => {
-    let cs = [];
-    let swe = [];
-    for (let i = 1; i < data_holder.length; i++) {
-      field = data_holder[i];
-      if (field.includes('CS')) {
-        cs.push(field[0]);
-      } else {
-        swe.push(field[0]);
+    rl.on('line', (row) => {
+      if (row.trim()) {
+        data_holder.push(row.split(","));
       }
-    }
-    console.log(`Number of students: ${data_holder.length - 1}`);
-    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-  });
+    });
+
+    rl.on('close', () => {
+      let cs = [];
+      let swe = [];
+      for (let i = 1; i < data_holder.length; i++) {
+        field = data_holder[i];
+        if (field.includes('CS')) {
+          cs.push(field[0]);
+        } else {
+          swe.push(field[0]);
+        }
+      }
+      console.log(`Number of students: ${data_holder.length - 1}`);
+      console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+      console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 }
