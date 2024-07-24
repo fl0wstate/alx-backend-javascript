57b7166641f363fe6ffd51d6fee58a15f1d1f80d
gh057 <victormurithi2020@gmail.com>
2024-07-24 16:19:00 +0300

    fix linting

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,10 @@
-const { response } = require('express');
 const http = require('http');
-const port = 1245;
 
+const port = 1245;
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-type' : 'text/plain' });
+  response.writeHead(200, { 'Content-type': 'text/plain' });
   response.end('Hello Holberton School!');
 });
 
 app.listen(port, () => {});
-
 module.exports = app;
