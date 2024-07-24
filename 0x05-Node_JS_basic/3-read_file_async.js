4cf321ac2b715d00c6fc5c68aa6811a03144f09a
gh057 <victormurithi2020@gmail.com>
2024-07-24 16:15:11 +0300

    fix

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,39 +5,33 @@ module.exports = async function countStudents(filepath) {
   return new Promise((resolve, reject) => {
     // Make a stream
     const stream = fs.createReadStream(filepath);
-  
-    stream.on('error', (_) => {
-      reject(new Error("Cannot load the database"));
+    stream.on('error', () => {
+      reject(new Error('Cannot load the database'));
     });
-  
     const rl = readline.createInterface({ input: stream });
     const dataHolder = [];
-  
     rl.on('line', (row) => {
       if (row.trim()) {
-        dataHolder.push(row.split(","));
+        dataHolder.push(row.split(','));
       }
     });
-  
     rl.on('close', () => {
-      let cs = [];
-      let swe = [];
-      for (let i = 1; i < dataHolder.length; i++) {
-        const field = dataHolder[i];
-        if (field.includes('CS')) {
-          cs.push(field[0]);
-        } else {
-          swe.push(field[0]);
+      const groupedData = [];
+      for (let i = 1; i < dataHolder.length; i += 1) {
+        const [firstName, lastName, age, field] = dataHolder[i];
+        if (!groupedData[field]) {
+          groupedData[field] = [];
         }
+        groupedData[field].push({ firstName, lastName, age });
       }
       console.log(`Number of students: ${dataHolder.length - 1}`);
-      console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-      console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-      resolve([
-          `Number of students: ${dataHolder.length - 1}`,
-          `Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`,
-          `Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`
-      ].join('\n'));
+      for (const field in groupedData) {
+        if (Object.hasOwnProperty.call(groupedData, field)) {
+          const studentNames = groupedData[field].map((students) => students.firstName);
+          console.log(`Number of students in ${field}: ${groupedData[field].length}. List: ${studentNames.join(', ')}`);
+          resolve(`Number of students in ${field}: ${groupedData[field].length}. List: ${studentNames.join(', ')}`);
+        }
+      }
     });
   });
-}
+};
