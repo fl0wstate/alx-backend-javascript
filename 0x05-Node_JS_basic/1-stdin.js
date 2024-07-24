4f8157189249e22e23f6589c157106dcdb975b6f
gh057 <victormurithi2020@gmail.com>
2024-07-24 05:53:38 +0300

    over the process alone

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,11 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.exit();
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  console.log(`Your name is: ${answer}`);
-  rl.close();
+process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
 });
-
-if (rl.input.isTTY) {
-  rl.on('close', () => {
-    console.log('This important software is now closing');
-    process.exit(0);
-  });
-}
