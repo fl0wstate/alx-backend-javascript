d3566c026d365b2422822b4eabb6cf366cfe971b
gh057 <victormurithi2020@gmail.com>
2024-07-24 05:20:31 +0300

    return an resovled object

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -30,10 +30,12 @@ module.exports = async function countStudents(filepath) {
           swe.push(field[0]);
         }
       }
-      console.log(`Number of students: ${dataHolder.length - 1}`);
-      console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-      console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-      resolve();
+      const result = [
+        `Number of students: ${dataHolder.length - 1}`,
+        `Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`,
+        `Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`
+      ].join('\n')
+      resolve(result);
     });
   });
 }
