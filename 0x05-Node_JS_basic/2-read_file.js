acb1b85f8aeef17337605cc963c3e6d99255ba50
gh057 <victormurithi2020@gmail.com>
2024-07-26 13:58:43 +0300

    while using sync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,20 +1,26 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
-module.exports = async function countStudents(filepath) {
+module.exports = function countStudents(filepath) {
   try {
-    const fileContent = await fs.readFile(filepath, 'utf8');
+    // Read file synchronously
+    const fileContent = fs.readFileSync(filepath, 'utf8');
+
+    // Split content into lines and filter out empty lines
     const lines = fileContent.split('\n').filter((line) => line.trim());
 
+    // Remove header and parse data
     const dataHolder = lines.slice(1).map((line) => line.split(','));
 
     console.log(`Number of students: ${dataHolder.length}`);
 
+    // Group data by field
     const groupedData = dataHolder.reduce((acc, [firstName, lastName, age, field]) => {
       if (!acc[field]) acc[field] = [];
       acc[field].push({ firstName, lastName, age });
       return acc;
     }, {});
 
+    // Log results
     for (const [field, students] of Object.entries(groupedData)) {
       const studentNames = students.map((student) => student.firstName);
       console.log(`Number of students in ${field}: ${students.length}. List: ${studentNames.join(', ')}`);
