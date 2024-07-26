c7beda95eb2800301ba948f3fc9bb455d90edab6
gh057 <victormurithi2020@gmail.com>
2024-07-26 19:21:14 +0300

    final results

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,20 +1,89 @@
 const exp = require('express');
-const students = require('./3-read_file_async');
+
 const app = exp();
 const port = 1245;
 
-app.get('/', (request, response) => {
+const fs = require('fs');
+const readline = require('readline');
+
+function countStudents(filepath) {
+  return new Promise((resolve, reject) => {
+    // Check if the file exists
+    fs.access(filepath, fs.constants.F_OK, (err) => {
+      if (err) {
+        return reject(new Error('Cannot load the database'));
+      }
+
+      const stream = fs.createReadStream(filepath);
+      const rl = readline.createInterface({ input: stream });
+      const dataHolder = [];
+      rl.on('line', (row) => {
+        if (row.trim()) {
+          dataHolder.push(row.split(','));
+        }
+      });
+      rl.on('close', () => {
+        const groupedData = {};
+        for (let i = 1; i < dataHolder.length; i += 1) {
+          const [firstName, lastName, age, field] = dataHolder[i];
+          if (!groupedData[field]) {
+            groupedData[field] = [];
+          }
+          groupedData[field].push({ firstName, lastName, age });
+        }
+        const result = [];
+        result.push(`Number of students: ${dataHolder.length - 1}`);
+        for (const field in groupedData) {
+          if (Object.hasOwnProperty.call(groupedData, field)) {
+            const studentNames = groupedData[field].map((students) => students.firstName);
+            result.push(`Number of students in ${field}: ${groupedData[field].length}. List: ${studentNames.join(', ')}`);
+          }
+        }
+        resolve(result);
+      });
+
+      // Handle read stream errors
+      stream.on('error', () => reject(new Error('Cannot load the database')));
+      return null;
+    });
+  });
+}
+
+async function handleStudentsRoute(response) {
+  const result = ['This is the list of our students'];
+  const filepath = process.argv[2] || '';
+  let body = '';
+
+  countStudents(filepath)
+    .then((data) => {
+      // handle the data
+      result.push(...data);
+      body = result.join('\n');
+      response.writeHead(
+        200,
+        { 'Content-Type': 'text/plain', 'Content-Length': body.length },
+      );
+      response.end(body);
+    })
+    .catch((err) => {
+      // handle the console.error();
+      result.push(err.message);
+      body = result.join('\n');
+      response.writeHead(
+        404,
+        { 'Content-Type': 'text/plain', 'Content-Length': body.length },
+      );
+      response.end(body);
+    });
+}
+
+app.get('/', (_, response) => {
   response.send('Hello Holberton School!');
 });
 
-app.get('/students', async (request, response) => {
-  try {
-    const result = await students(process.argv[2]);
-    response.send(result);
-  } catch(err) {
-    console.log(err);
-  }
+app.get('/students', async (_, response) => {
+  await handleStudentsRoute(response);
 });
-app.listen(port, () => {});
 
+app.listen(port, () => {});
 module.exports = app;
