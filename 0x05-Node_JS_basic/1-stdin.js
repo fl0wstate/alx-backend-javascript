f58cf4e332b9d31b10124886a4165b8fc584e27a
gh057 <victormurithi2020@gmail.com>
2024-07-26 21:20:48 +0300

    call it as a functoin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+function userInput() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+    if (!process.stdin.isTTY) {
+      process.stdout.write('This important software is now closing\n');
+    }
+    process.exit();
+  });
+}
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
-  }
-  
-  process.exit();
-});
+userInput();
