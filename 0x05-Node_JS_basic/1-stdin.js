0d5ee9d9f6c06dbd000dcb8e10b47540da39d9a6
gh057 <victormurithi2020@gmail.com>
2024-09-17 13:38:09 +0300

    helper files for the project

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+/**
+ * How to make a simple stdin
+ * Catch you should be able to tell the difference between
+ * a pipped command and a normal command
+ * Learn to use the child process
+ * Allows you to handle multiple process
+ */
+
