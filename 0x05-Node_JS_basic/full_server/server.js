0e337f473c4886a96f0e709f2e24fee803ed4c50
gh057 <victormurithi2020@gmail.com>
2024-09-18 02:47:10 +0300

    handling requirement fail

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -2,17 +2,10 @@
 import express from 'express';
 import mapRoutes from './routes/index';
 
-/**
- * Create an express server and map routes
- * @type {express}
- */
-
 const app = express();
 const port = 1245;
 
 mapRoutes(app);
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+app.listen(port, () => {});
 
 export default app;
