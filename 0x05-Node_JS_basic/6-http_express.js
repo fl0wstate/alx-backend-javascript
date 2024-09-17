4d53bb856c73dad558242685b3851a9f5dd3c1c9
gh057 <victormurithi2020@gmail.com>
2024-09-18 02:26:59 +0300

    using express js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+/**
+ * handling request with express server
+ */
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {});
+
+module.exports = app;
