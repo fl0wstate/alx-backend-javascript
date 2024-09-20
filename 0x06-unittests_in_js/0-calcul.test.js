5f6c6d12a35a25ddb4ad5489c2926de8f8de80ab
gh057 <victormurithi2020@gmail.com>
2024-09-20 14:16:30 +0300

    fixing the testcases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -26,7 +26,7 @@ describe('calculateNumber', function() {
   });
 
   it('should handle very small numbers', function() {
-    assert.strictEqual(calculateNumber(0.0000001, 0.0000002), 1);
+    assert.strictEqual(calculateNumber(0.0000001, 0.0000002), 0);
   });
 
   it('should throw TypeError for string input', function() {
@@ -60,4 +60,27 @@ describe('calculateNumber', function() {
   it('should handle Infinity', function() {
     assert.strictEqual(calculateNumber(Infinity, 5), Infinity);
   });
+
+  it('it round the first argument', () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
+  });
+
+  it('it round the second argument', () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
+  });
+
+  it('it should return the right number', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
+  });
+
+
 });
