821c7d30e6b7495d875ed489b1fa5f60d77f03f7
gh057 <victormurithi2020@gmail.com>
2024-09-20 14:16:13 +0300

    usign Math.round

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,8 +1,7 @@
 function calculateNumber(a, b) {
   if (typeof a === 'number' && typeof b === 'number' && !isNaN(a) && !isNaN(b)) {
-    return Math.ceil(a + b);
+    return (Math.round(a) + Math.round(b));
   }
   throw new TypeError('Both params should be of type integers');
 }
-
 module.exports = calculateNumber;
